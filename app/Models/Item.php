08f28a8cd3fab5e2ec4dbc698f0c61987095faa5
geri35 <geri@upi.edu>
2024-11-25 20:54:42 +0700

    inisialisasi-projek

---
+++ app/Models/Item.php
@@ -0,0 +1,21 @@
+<?php
+
+// app/Models/Item.php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'price'];
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class)
+                    ->withPivot('quantity', 'price');
+    }
+}
---
+++ app/Models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+// app/Models/Order.php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['customer_name', 'total_amount'];
+
+    public function items()
+    {
+        return $this->belongsToMany(Item::class)
+                    ->withPivot('quantity', 'price');
+    }
+}
